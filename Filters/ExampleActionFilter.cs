5cf4f155ec999f2312f47daf42958591b1b98bbb
maxefimov <efimov.top.academy@gmail.com>
2023-03-19 19:18:25 +0300

    Добавьте файлы проекта.

---
+++ Filters/ExampleActionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MvcFilterPipesApp.Filters
+{
+    public class LastJoinActionFilter : Attribute, IResourceFilter //, IOrderedFilter
+    {
+        //public int Order { get; }
+        //public LastJoinActionFilter(int order) => Order = order;
+
+        string email;
+
+        public LastJoinActionFilter(string email)
+        {
+            this.email = email;
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.HttpContext.Response.Cookies.Append("LastJoin", DateTime.Now.ToString("dd.MM.yyyy HH-mm-ss"));
+            context.HttpContext.Response.Cookies.Append("Email", email);
+        }
+    }
+}
---
+++ Filters/IEFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.RegularExpressions;
+
+namespace MvcFilterPipesApp.Filters
+{
+    public class IEFilter : Attribute, IAsyncResourceFilter
+    {
+        public async Task OnResourceExecuting(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            string browser = context.HttpContext.Request.Headers["User-Agent"].ToString();
+            if (Regex.IsMatch(browser, "MSIE|Trident"))
+                context.Result = new ContentResult() { Content = "Your browser is old" };
+            else
+                await next();
+        }
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            await next();
+        }
+    }
+}
